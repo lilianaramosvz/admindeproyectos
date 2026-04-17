5e693298e140b21e4a48987bd1341e2bf012df4a
Anna Sofia <A00574296@tec.mx>
2026-04-17 15:49:04 -0600

    localhost

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 //frontend\src\services\api.js
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://sammy-ulfh.dev";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
 
 async function getJson(path) {
   const res = await fetch(`${BASE_URL}${path}`);
